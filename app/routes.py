371cd3c97008dc9c6f43794d2ea874859f06bffb
Bilal Goumar <bilal.goumar@etu.emse.fr>
2022-02-19 18:31:58 +0100

    deploy to heroku

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
+from flask import jsonify
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello World"
+
+@app.route('/restaurant')
+def restaurant():
+    return jsonify({"bonjour":1})
